9606b66b5dd2ff7e827c17f538e9d782f57f2a71
Grant Innes <grybukas90@gmail.com>
2023-04-02 21:54:44 +0300

    Simple Python quiz program
    V1 of python quiz program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Quiz
 Simple quiz program
+## Python Quiz program
+This is my first python program, it is a simple and easy python quiz to practice using: functions, variables, loops and if statememnts.
---
+++ main.py
@@ -0,0 +1,34 @@
+questions = ['\nIs Python named after \'Monty Python\'?',
+             'Is indentation mandatory in Python?',
+             'Is Python the hardest programming language?',
+             'Did Python influence the design of Java?',
+             'Do pythons swallow their prey whole?']
+
+answers = ['yes', 'yes', 'no', 'no', 'yes']
+
+print('\n\n* * * * * * * * * * * * * * * * * * * *\nWelcome To The Python Quiz! \nPlease answer \'yes\' or \'no\' to each question.\n* * * * * * * * * * * * * * * * * * * *\n') #Welcome message before main function starts
+
+def start(): #Function to prompt the user to start the quiz
+
+    ready = input('Are you ready? \n')
+    if ready == 'yes': #If condition is true, main function will run
+        quizGame()
+    else: 
+        start() #loops back to start function
+        
+def quizGame():  #Main function for the quiz
+
+    score = 0 #Score variable initialized with zero (declared locally withing function)
+
+    for i in range(len(questions)): #'range defines how many times to run the loop (len). 'len' = how many items in the questions variable
+        print(questions[i]) 
+        ans = input('Your answer: \n') #takes answer from user & stores in variable 'ans'
+        if ans == answers[i]: ## check if answer is correct by matching item in 'answers list using if statement
+            print('\nCorrect!!! Well Done!\n---------------------\n') # will run if above condition is true
+            score += 1 # increases score variable by 1 if correct
+        else:  # code to run if condition is false
+            print('\nIncorrect!!!\n************\n')
+
+    print(f'You scored a total of: {score}!!!\nThank You For Playing!\n\n') #takes total score of answeered questions & displays in message to user
+
+start() #calls the first function after welcome message is displayed
